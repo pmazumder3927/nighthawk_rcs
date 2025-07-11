83e910d451e987e281b15a4e30603a6137555e1b
pmazumder3927 <pmazumder3927@gmail.com>
2025-07-10 19:02:47 -0700

    revert accidental cupy rollback

--- src/__init__.py
+++ src/__init__.py
@@ -5,13 +5,6 @@ A simulation environment for radar cross-section optimization
 inspired by stealth aircraft design principles.
 """
 
-# 2D modules (original)
-from .rcs_calc import RCSCalculator
-from .geometry import Geometry2D, Geometry3D
-from .optimization import TopologyOptimizer
-from .visualization import RCSVisualizer
-
-# 3D modules (new)
 from .rcs_calc_3d import RCS3DCalculator
 from .geometry_3d import Geometry3D as Geometry3DAdvanced
 from .optimization_3d import TopologyOptimizer3D
